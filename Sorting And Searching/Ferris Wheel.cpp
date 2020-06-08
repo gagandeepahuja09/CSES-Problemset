03b4be2585d9c8da2ff37a15b4817ee5def53a7a
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-06-08 16:47:50 +0530

    easy

---
+++ Sorting And Searching/Ferris Wheel.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long int
+#define vi vector<int>
+#define vvi vector<vi>
+#define read(a)  for(int i = 0; i < n; i++) cin >> a[i];
+#define print(a)  for(int i = 0; i < n; i++) cout << a[i] << " ";
+#define pb push_back
+#define pql priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define pqlv priority_queue<vi>
+#define pqsv priority_queue<vi, vvi, greater<vi>>
+#define endl '\n'
+#define N 1234567
+#define MOD 1000000007
+ 
+signed main() {
+    int n, x;
+    cin >> n >> x;
+    vi a(n);
+    read(a);
+    sort(a.begin(), a.end());
+    int cnt = 0;
+    int i = 0, j = n - 1;
+    while(i <= j) {
+        if(a[i] + a[j] <= x) {
+            i++; j--;
+        }
+        else {
+            j--;
+        }
+        cnt++;
+    }
+    cout << cnt << endl;
+}
